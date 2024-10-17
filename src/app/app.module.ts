13720666cd253780c6a750b6439cebe60a7d4832
mazeneko <46620955+mazeneko@users.noreply.github.com>
2024-10-17 23:45:53 +0900

    add bluesky

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { BlueskyIconComponent } from './icon/bluesky-icon/bluesky-icon.component';
 import { DiscordIconComponent } from './icon/discord-icon/discord-icon.component';
 import { GithubIconComponent } from './icon/github-icon/github-icon.component';
+import { InstagramIconComponent } from './icon/instagram-icon/instagram-icon.component';
 import { MisskeyIconComponent } from './icon/misskey-icon/misskey-icon.component';
 import { RecycleIconComponent } from './icon/recycle-icon/recycle-icon.component';
+import { SteamIconComponent } from './icon/steam-icon/steam-icon.component';
+import { ThreadsIconComponent } from './icon/threads-icon/threads-icon.component';
 import { TwitterIconComponent } from './icon/twitter-icon/twitter-icon.component';
-import { LinksComponent } from './links/links.component';
-import { TitleComponent } from './title/title.component';
 import { LinkButtonComponent } from './links/link-button/link-button.component';
+import { LinksComponent } from './links/links.component';
 import { NoLinkButtonComponent } from './links/no-link-button/no-link-button.component';
 import { ProfileComponent } from './profile/profile.component';
-import { SteamIconComponent } from './icon/steam-icon/steam-icon.component';
-import { InstagramIconComponent } from './icon/instagram-icon/instagram-icon.component';
-import { ThreadsIconComponent } from './icon/threads-icon/threads-icon.component';
-import { GalleryComponent } from './gallery/gallery.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TwitterIconComponent,
+    BlueskyIconComponent,
     GithubIconComponent,
     DiscordIconComponent,
     MisskeyIconComponent,
---
+++ src/app/icon/bluesky-icon/bluesky-icon.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bluesky-icon',
+  templateUrl: './bluesky-icon.component.html',
+  styleUrls: ['./bluesky-icon.component.scss']
+})
+export class BlueskyIconComponent {
+}
